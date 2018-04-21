b76bfe95158b9dfead9a861fb6cf0c1f3ed22d7a
randomagente <fDfQ6gAuEZ@protonmail.com>
2018-04-21 22:53:39 +0100

    imlemented lists with double links

--- main.c
+++ main.c
@@ -27,74 +27,130 @@ typedef struct edge{
     int flow;
     int cap; // Capacity
     int rev; //https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/
-} Edge;
+} *Edge;
 
 typedef struct node{
     Edge e;
+    struct node *prev;
     struct node *next;
-} Link;
+} *Link;
 
-Link **adj_list;
+/*
+ * List Operations
+ */
+
+typedef struct list_t {
+  Link head;
+  int size;
+  Link back;
+} list_t; // list type
+
+list_t* init_list(){
+  list_t* new_list = (list_t*) malloc(sizeof(list_t));
+  new_list->head = NULL;
+  new_list->size = 0;
+  new_list->back = NULL;
+  return new_list;
+}
+
+void push_front(list_t *list, Edge e){
+    Link new = (Link) malloc(sizeof(struct node));
+    new->e = e;
+
+    if (list->head == NULL){
+      new->next = NULL;
+      list->back = new;
+    } else {
+      new->next = list->head;
+      list->head->prev = new;
+    }
+    new->prev = NULL;
+    list->head = new;
+    list->size += 1;
+}
+
+void push_back(list_t *list, Edge e){
+  Link new = (Link) malloc(sizeof(struct node));
+  new->e = e;
+  new->next = NULL;
+  if (list->back == NULL) {
+    new->prev = NULL;
+    list->head = new;
+  } else {
+    new->prev = list->back;
+    list->back->next = new;
+  }
+  list->back = new;
+  list->size += 1;
+}
+
+void print_list(list_t *list){
+  printf("printing list: ");
+  Link l;
+  for (l=list->head; l!=NULL; l=l->next) printf("%d>", l->e->id);
+  printf("\n");
+}
+
+void print_list_reverse(list_t *list){
+  printf("printing list: ");
+  Link l;
+  for (l=list->back; l!=NULL; l=l->prev) printf("%d>", l->e->id);
+  printf("\n");
+}
+
+
+/*
+ * Queue Operations
+ */
+
+/*
+ *  Graph Operation
+ */
+list_t **adj_list;
 int *level;
 
-void init_list(Link *l){
-    l = NULL;
-}
-
-Link* push_end(Link *l, Edge e){
-    Link *new;
-    new = (struct node *) malloc(sizeof(struct node));
-    new->e = e;
-    new->next = l;
-    l = new;
-    return l;  // tenho que devolver o l porque nao e global;
-}
-
 void init_graph(int vertex){
-    adj_list = (struct node **)malloc(sizeof(struct node *)*(vertex+2));     // A source vai ser 0 e o target vai ser V+1
-    for (int id=0; id < vertex+2; id++) {
-        init_list(adj_list[id]);   // Mete todos os ponteiro do array a NULL; Ou seja inicializa as listas
+    adj_list = (list_t **) calloc(vertex+1, sizeof(list_t*));     // A source vai ser 0 e o target vai ser V+1
+    for (int id=0; id < vertex+1; id++) {
+        adj_list[id] = init_list();   // Mete todos os ponteiro do array a NULL; Ou seja inicializa as listas
     }
+}
+
+Edge create_edge(int id, int cap){
+  Edge e = (struct edge*) malloc(sizeof(struct edge));
+  e->id = id;
+  e->flow = 0;
+  e->cap = cap;
+  /*e->rev = length(adj_list[v]);*/
+  return e;
 }
 
 
 void addEdge(int u, int v, int C){
-    Edge a = (struct edge)malloc(sizeof(struct edge *));
-    Edge b = (struct edge)malloc(sizeof(struct edge *));
+  Edge a = create_edge(v,C);
+  Edge b = create_edge(u,C);
 
-    a.id = v;
-    a.flow = 0;
-    a.cap = C;
-//    a.rev = length(adj_list[v]);
-
-    b.id = u;
-    b.flow = 0;
-    b.cap = C;
-//    b.rev = length(adj_list[v]);
-
-    adj_list[u] = push_end(adj_list[u], a);
-    adj_list[v] = push_end(adj_list[u], b); //reverse
+  push_back(adj_list[u], a);
+  push_back(adj_list[u], b); //reverse
 }
 
 
 int main(){
-    int V;
-    int m;          //linhas
-    int n;          //colunas
-    scanf("%d", &m);
-    scanf("%d", &n);
+    int m; /*linhas*/
+    int n; /*colunas*/
+    int i, begin, end;
 
-    V = m*n;        //Numero de Vertices
+    /* input 1: dims da matriz */
+    scanf("%d %d", &m, &n);
 
-   /* for (int i=0; i < edges; i++) {
+    /* initialize graph with +2 vertices for the source and target*/
+    init_graph(m*n+2);
+
+    for (i=0; i < m; i++) {
    	    scanf("%d %d", &begin, &end);
-        add_edge(begin, end);         //Comeca a construir o grafo segundo o input
-       }*/
-
-    init_graph(V);
-
+        addEdge(begin, end, 999999);
+    }
 }
 
 
 /*HELLO WORLD*/
-
