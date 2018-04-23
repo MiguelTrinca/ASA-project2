00f9dcfd4dff0f8d146e257bd836fb73ce7ad2c4
MiguelTrinca <miguel.trinca@tecnico.ulisboa.pt>
2018-04-23 12:39:35 +0100

    Ler o TODO!
    Ja leio os 3 primeiros inputs, falta o resto. Da para fazer uma otimizacao logo no inicio. Nao percebo muito bem
    porque e que temos duas listas.

--- main.c
+++ main.c
@@ -3,22 +3,16 @@
 #include <math.h>
 
 /*
-  Fiz cenas so para te ensinar a usar git
+  Otimizacao! Quando estamos a ler o input podemos logo descobrir os caminhos e flows de distancia 3.
 */
-// Ver aulas 10 e 11 onde tem codigo de ford-fulkerson e edmonds-karp
-// Ver aula 7 onde esta a BFS
-
-//Nota: O Edmonds-Karp e um ford-fulkerson, no entanto temos que fazer BFS para descobrir os caminhos de aumento,
-//  isto e os caminhos mais curtos
-
-
-//Para implementar tanto EK ou Dinitz temos que fazer uma BFS, portanto Fazer isso.
 
 /*TODO
-    Defenir a minha lista de adjacencias
-    Ler o input
-    Fazer BFS
-    Fazer Dinics
+    Ler o input 4 e 5 o 4 esta a dar problemas
+    Implementar a otimizacao
+    Fazer funcoes para a Queue. (Ir ver a funcao BFS ver que funcoes e que precisa)
+    BFS esta quase so precisa da Queue.
+    SendFlow
+    MaxFlow
 */
 
 
@@ -35,15 +29,26 @@ typedef struct node{
     struct node *next;
 } *Link;
 
-/*
- * List Operations
- */
 
 typedef struct list_t {
   Link head;
   int size;
   Link back;
 } list_t; // list type
+
+/*
+ * Global
+ */
+
+
+list_t **adj_list;
+int *level;
+int V;
+
+/*
+ * List Operations
+ */
+
 
 list_t* init_list(){
   list_t* new_list = (list_t*) malloc(sizeof(list_t));
@@ -103,14 +108,15 @@ void print_list_reverse(list_t *list){
  * Queue Operations
  */
 
+
+
 /*
  *  Graph Operation
- */
-list_t **adj_list;
-int *level;
+ */  
 
 void init_graph(int vertex){
     adj_list = (list_t **) calloc(vertex+1, sizeof(list_t*));     // A source vai ser 0 e o target vai ser V+1
+    level = (int *) malloc(sizeof(int)*vertex);
     for (int id=0; id < vertex+1; id++) {
         adj_list[id] = init_list();   // Mete todos os ponteiro do array a NULL; Ou seja inicializa as listas
     }
@@ -127,6 +133,8 @@ Edge create_edge(int id, int cap){
 
 
 void addEdge(int u, int v, int C){
+  /*Quando fazemos addEdge temos que por o backward edge tambem. Acho que esta implementacao so poe
+  um vez*/
   Edge a = create_edge(v,C);
   Edge b = create_edge(u,C);
 
@@ -134,23 +142,97 @@ void addEdge(int u, int v, int C){
   push_back(adj_list[u], b); //reverse
 }
 
+/*
+ * Dinic's Algorithm
+ */
+
+/*
+int BFS(int s, int t){
+  for (int i = 0 ; i < V ; i++)
+      level[i] = -1;
+
+  level[s] = 0;  // Level of source vertex
+ 
+   Criar a queue Q
+  list< int > q;
+  q.push_back(s);
+ 
+
+  
+  while (!empty(Q)){
+    int u = front(Q);
+    pop_front(Q);
+    for (l = adj[u]->head; i != NULL; l=l->next){
+      Edge e = l->e;
+      if (level[e.id] < 0  && e.flow < e.cap){
+                // Level of current vertex is,
+                // level of parent + 1
+        level[e.id] = level[u] + 1;
+ 
+        push_back(Q, e.id);
+        }
+      }
+    }
+    // IF we can not reach to the sink we
+    // return false else true
+  return level[t] < 0 ? 0 : 1 ;
+} */
 
 int main(){
     int m; /*linhas*/
     int n; /*colunas*/
-    int i, begin, end;
+    int i, j, cap;
 
     /* input 1: dims da matriz */
     scanf("%d %d", &m, &n);
 
-    /* initialize graph with +2 vertices for the source and target*/
-    init_graph(m*n+2);
+    /* Number of vertexs*/    
+    V = m*n + 2;
 
-    for (i=0; i < m; i++) {
-   	    scanf("%d %d", &begin, &end);
-        addEdge(begin, end, 999999);
+    /* initialize graph with +2 vertices for the source and target*/
+    init_graph(V);
+
+    /* input 2: capacidade dos vertices da source (pretos) */
+    for(i=1; i < V-1; i++){
+      scanf("%d", &cap);
+      addEdge(0, i, cap);
     }
+  
+    /* input 3: capacidade dos vertices do target (pretos) */
+    for(i=1; i < V-1; i++){
+      scanf("%d", &cap);
+      addEdge(V, i, cap);
+      
+      /*  Otimizacao: Mandar logo o fluxo total. (pois este caminho {s,i,t} e o menor caminho)
+      1 - Comparar a capacidade lida do vertice source -> i e i->target.
+      2 - Mandar o flow total da capacidade minima.
+      
+     if(adj_list[V].head.e.cap <= adj_list[0].head.e.cap){
+       //sendFlow(cap);
+     }
+     else if(adj_list[V].head.e.cap >= adj_list[0].head.e.cap){
+       //sendFlow(adj_list[0].head.e.cap);       
+     }*/
+   
+    }
+
+    /* input 4: capacidade entre vertices na horizontal */
+    //Esta a dar segfault
+    i=1;
+    j=1;
+    while(i < n){
+      scanf("%d", &cap);
+      addEdge(j, j+1, cap);
+      if(i == n-1 && j != V-2){
+        i = 1;
+        j *= 2;
+      }
+      i++;
+      j++;
+    }
+    
+    /* input 5: capacidade entre vertices na vertical */
+    
 }
 
 
-/*HELLO WORLD*/
